402eb2db1c3ef9ac6016cc540e1526ea34df5439
Whiplash_zeb <blgszeb@outlook.com>
2018-04-10 16:50:20 +0800

    extract fundamental feature

--- extraction/inner_sentence.py
+++ extraction/inner_sentence.py
@@ -6,7 +6,7 @@ CID = {}
 
 
 # 读取全部的CID关系
-def ReadCID(file):
+def read_CID(file):
     with open(file) as fp:
         line = fp.readline()
         while len(line) > 0:
@@ -20,7 +20,7 @@ def ReadCID(file):
 
 
 # 抽取特征函数
-def ReadSentence(raw_file, feature_file):
+def read_sentence(raw_file, feature_file):
     fw = open(feature_file, 'w')
 
     # 0 标记标题 1 标记摘要
@@ -43,37 +43,41 @@ def ReadSentence(raw_file, feature_file):
                     elif line.startswith("abstract"):
                         title_flag = 1
                         line = line[11:]
-                # 识别出全部共现的实体对
-                if ContainEntity(line):
-                    l = len(line)
-                    all_entities = ExtractAllEntities(line)
-                    line = line.split()
-                    # 处理每一对实体
-                    for key, value in all_entities.items():
-                        chemical_pos = key[0][0]  # 化学物质位置
-                        chemical = key[0][1]
-                        disease_pos = key[1][0]  # 疾病位置
-                        disease = key[1][1]
-                        is_cid = value  # 是否为正例
-                        distance = abs(chemical_pos - disease_pos) / l  # 距离
-                        order = 0  # 化学物质在前
-                        if chemical_pos > disease_pos:
-                            order = 1
-                        if order == 0:
-                            others = ContainOthers(line[chemical_pos + 1:disease_pos])
-                        else:
-                            others = ContainOthers(line[disease_pos + 1:chemical_pos])
-                        other_chemical_number = others[0]
-                        other_disease_number = others[1]
-                        other_chemical_kind = others[2]
-                        other_disease_kind = others[3]
+                    # 识别出全部共现的实体对
+                    if contain_entity(line):
+                        all_entities = extract_all_entities(line)
+                        line = line.split()
+                        l = len(line)
+                        # 处理每一对实体
+                        # 特征顺序：CID,化学物质位置,疾病位置,距离,顺序
+                        for key, value in all_entities.items():
+                            chemical_pos = key[0][0]  # 化学物质位置
+                            chemical = key[0][1]
+                            disease_pos = key[1][0]  # 疾病位置
+                            disease = key[1][1]
+                            is_cid = value  # 是否为正例
+                            distance = abs(chemical_pos - disease_pos) / l  # 距离
+                            order = 0  # 化学物质在前
+                            if chemical_pos > disease_pos:
+                                order = 1
+                            if order == 0:
+                                others = contain_others(line[chemical_pos + 1:disease_pos])
+                            else:
+                                others = contain_others(line[disease_pos + 1:chemical_pos])
+                            other_chemical_number = others[0]
+                            other_disease_number = others[1]
+                            other_chemical_kind = others[2]
+                            other_disease_kind = others[3]
+                            fw.write("%d %d %d %f %d %d %d %d %d\t%s\t%s\n" % (
+                                is_cid, chemical_pos, disease_pos, distance, order, other_chemical_number,
+                                other_disease_number, other_chemical_kind, other_disease_kind, chemical, disease))
                 line = fp.readline()
             passage = line
             fw.write(passage)
 
 
 # 判断句子内是否存在实体对
-def ContainEntity(sentence):
+def contain_entity(sentence):
     sentence = sentence.split()
 
     contain_chemical = False
@@ -93,7 +97,7 @@ def ContainEntity(sentence):
 
 # 从共现的句子内抽取全部的实体对:
 # 返回格式：((pos,chemical),(pos,disease)):CID
-def ExtractAllEntities(sentence):
+def extract_all_entities(sentence):
     # 存储化学物质实体，疾病实体，结果集合
     chemical_entities = []
     disease_entities = []
@@ -123,7 +127,7 @@ def ExtractAllEntities(sentence):
 
 
 # 统计实体对间包含的其他化学物质，疾病数量及种类
-def ContainOthers(sentence):
+def contain_others(sentence):
     chemical = set()
     disease = set()
     chemical_number = 0
@@ -142,8 +146,6 @@ def ContainOthers(sentence):
             if match:
                 disease.add(word)
                 disease_number += 1
-    print(chemical)
-    print(disease)
     res = (chemical_number, disease_number, len(chemical), len(disease))
     return res
 
@@ -152,6 +154,7 @@ if __name__ == "__main__":
     raw_file_list = ["replace/train.txt", "replace/develop.txt", "replace/test.txt"]
     feature_file_list = ["feature/train.txt", "feature/develop.txt", "feature/test.txt"]
 
-    sentence = "In brain membranes from spontaneously D_D006973 rats C_D003000 , 10 ( -8 ) to 10 ( -5 ) M , did not influence stereoselective binding of [ 3H ] -C_D009270 ( 8 nM ) , and C_D009270 , 10 ( -8 ) to 10 ( -4 ) M , did not influence C_D003000-suppressible binding of C_-1 ( 1 nM ) ."
-    ReadCID("replace/train.txt")
-    print(ContainOthers(sentence.split()))
+    # sentence = "In brain membranes from spontaneously D_D006973 rats C_D003000 , 10 ( -8 ) to 10 ( -5 ) M , did not influence stereoselective binding of [ 3H ] -C_D009270 ( 8 nM ) , and C_D009270 , 10 ( -8 ) to 10 ( -4 ) M , did not influence C_D003000-suppressible binding of C_-1 ( 1 nM ) ."
+    read_CID("replace/train.txt")
+    for raw_file, feature_file in zip(raw_file_list, feature_file_list):
+        read_sentence(raw_file, feature_file)
